d594829a9edbe85a694e92a840cc496f0189343d
en <eridhobffry@yahoo.com>
2019-12-18 11:42:02 +0100

    function 2

--- assets/js/script-extend.js
+++ assets/js/script-extend.js
@@ -14,6 +14,22 @@ $('.optionBox-reason').on('click','.remove',function() {
  	$(this).parent().parent().parent().remove();
 });
 
+$('#add-return-condition').click(function() {
+    $('.block-conditions:last').before('<div class="block-conditions section-bottom-20">' +
+    '<div class="range">'+
+        '<div class="cell-lg-10">'+
+        '<input class="form-control" value="" id="retourneoConditions" type="text" name="retourneoConditions">'+
+        '</div>'+
+        '<div class="cell-lg-2 text-left">'+
+        '<span class="remove-condition"><i class="fa fa-trash text-red"></i></span>'+
+        '</div>'+
+    '</div>'+
+    '</div>');
+});
+$('.optionBox-condition').on('click','.remove-condition',function() {
+ 	$(this).parent().parent().parent().remove();
+});
+
 $( "#next-to-2" ).click(function() {
     $( "#trigger-2" ).trigger( "click" );
   });
