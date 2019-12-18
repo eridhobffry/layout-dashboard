71f23bf93f893444440fb56fec744140f5a59a30
en <eridhobffry@yahoo.com>
2019-12-18 12:21:27 +0100

    source mapping

--- assets/js/script-extend.js
+++ assets/js/script-extend.js
@@ -18,7 +18,7 @@ $('#add-return-condition').click(function() {
     $('.block-conditions:last').before('<div class="block-conditions section-bottom-20">' +
     '<div class="range">'+
         '<div class="cell-lg-10">'+
-        '<input class="form-control" value="" id="retourneoConditions" type="text" name="retourneoConditions">'+
+        '<input class="form-control" value="" id="retourneConditions" type="text" name="retourneConditions">'+
         '</div>'+
         '<div class="cell-lg-2 text-left">'+
         '<span class="remove-condition"><i class="fa fa-trash text-red"></i></span>'+
@@ -28,6 +28,26 @@ $('#add-return-condition').click(function() {
 });
 $('.optionBox-condition').on('click','.remove-condition',function() {
  	$(this).parent().parent().parent().remove();
+});
+
+
+$('#add-return-mapping').click(function() {
+    $('.block-mapping:last').before('<div class="block-mapping section-bottom-20">' +
+    '<div class="range">'+
+    '<div class="cell-lg-6 cell-xs-12">'+
+        '<label class="form-label form-label-outside" for="name">Source</label>'+
+        '<input class="form-control" value="" id="sourceShipmentMapping" type="text" name="sourceShipmentMapping">'+
+    '</div>'+
+    '<div class="cell-lg-6 cell-xs-12">'+
+        '<label class="form-label form-label-outside" for="name">Target</label>'+
+        '<input class="form-control" value="" id="targetShipmentMapping" type="text" name="targetShipmentMapping">'+
+    '</div>'+
+    '</div>' +
+    '<span class="remove-mapping"><i class="fa fa-trash text-red"></i></span>'+
+    '</div>');
+});
+$('.optionBox-mapping').on('click','.remove-mapping',function() {
+ 	$(this).parent().remove();
 });
 
 $( "#next-to-2" ).click(function() {
