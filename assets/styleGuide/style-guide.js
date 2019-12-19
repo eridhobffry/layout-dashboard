9ac26a51891ab12656a37ff2ec09aee6a39bfcd5
eridhobffry <eridhobffry@github.com>
2019-12-19 13:01:36 +0100

    arrange

---
+++ assets/styleGuide/style-guide.js
@@ -0,0 +1,11 @@
+// ODC Style Guide
+
+export let colorDeafault1 = "#e0ab22"; // yellow
+export let colorDeafault2 = "#213c5f"; // blue
+export let fontFamily = "Raleway";
+export let buttonRadius = "100em";
+export let cardRadius = "0px";
+export let cardShadow = "0px 0px 20px 0px rgba(0,0,0,0.1)";
+export let iconFamily = "fontAwesome";
+export let spaceNewContent_Range1 = "20px"; // padding top || margin-bottom
+export let spaceNewContent_Range1 = "30px"; // padding top || margin-bottom
